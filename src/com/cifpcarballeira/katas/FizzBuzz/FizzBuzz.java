0f7c0ec6215efe899e801ac4614c15b5d4eed81b
Julio Mosquera <julio.mosquera@yahoo.es>
2019-02-20 12:22:33 +0100

    5 devuelve Buzz

--- src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -12,9 +12,9 @@ package com.cifpcarballeira.katas.FizzBuzz;
 public class FizzBuzz {
 
     public String getFizzBuzz(int num) {
-        if (num%3==0) return "Fizz";
-        if (num==1) return "1";
-        else return "2";  
+        if (num%3 == 0) return "Fizz";
+        if (num%5 == 0) return "Buzz";
+        else return Integer.toString(num);
     }
     
 }
--- test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
+++ test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
@@ -39,4 +39,12 @@ public class TestFizzBuzz {
         String result = fb.getFizzBuzz(3);
         assertEquals("Fizz", result);
     }
+    
+     @Test
+    public void test5devuelveBuzz() {
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.getFizzBuzz(5);
+        assertEquals("Buzz", result);
+    }
+    
 }
