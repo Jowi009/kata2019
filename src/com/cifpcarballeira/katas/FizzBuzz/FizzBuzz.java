a14dd1e0b2631ab72742a163907cab334750ed6c
Julio Mosquera <julio.mosquera@yahoo.es>
2019-02-20 10:53:54 +0100

    3 devuelve Fizz

--- src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -12,8 +12,9 @@ package com.cifpcarballeira.katas.FizzBuzz;
 public class FizzBuzz {
 
     public String getFizzBuzz(int num) {
+        if (num%3==0) return "Fizz";
         if (num==1) return "1";
-        else return "2"; 
+        else return "2";  
     }
     
 }
